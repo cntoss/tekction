c7d79d84ec97df2c5edb46d68e5afb4815f23aa4
cntoss77 <santosh.adhikari@tekkon.com.np>
2022-12-26 15:45:25 +0545

    firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tekction/locator.dart';
 import 'package:tekction/navigation/router.gr.dart';
 import 'package:tekction/utils/theme_data.dart';
 
+import 'firebase_options.dart';
+
 void main() {
   setLocator();
+  initializeDefault();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
+}
+
+Future<void> initializeDefault() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print('Initialized default app');
 }
 
 class MainApp extends StatefulWidget {
